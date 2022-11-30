60bac79d51bcf8d85b0ad92e88b6b66f688c75f3
Ronak Patel <ronak2715@gmail.com>
2022-11-29 20:39:31 -0500

    app.py updated

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-
-
 from flask import Flask, render_template
 import os
 import requests
@@ -14,3 +12,43 @@ load_dotenv(find_dotenv()) # This is to load your API keys from .env
 # declaring app name
 app = Flask(__name__)
  
+# defining home page   
+@app.route('/')
+def index():
+
+    #Artist ID from spotify 
+    artists=["4YRxDV8wJFPHPTeXepOstw", "3xU8YsNNkmWSPewlB18NUz", "5f4QpKfy7ptCHwTqspnSJI","6vWDO969PvNqNYHIOW5v0m"]
+    rand_id = random.choice(artists)
+    
+    try: 
+        artistInfo = getArtistInfo(rand_id)
+        tracks = getTracks(rand_id)
+        
+    except:
+        artistInfo = gettestArtistInfo()
+        tracks = gettestTracks()
+   
+    trackList = []
+    for track in tracks:
+        trackList.append(track)
+        
+    lyrics = []
+    for track in trackList:
+        lyrics.append("http://genius.com" + getLyrics(track["name"], artistInfo["name"]))
+    
+    #returning index.html
+    return render_template(
+        "index.html",
+        artistImg =artistInfo["images"][0]["url"],
+        artistName =artistInfo["name"],
+        tracks=getTracks(rand_id),
+        tracksLen = len(tracks),
+        lyrics=lyrics,
+        )
+
+# running app 
+app.run(
+    port=int(os.getenv('PORT', 8080)),
+    host=os.getenv('IP', '0.0.0.0'),
+    debug=True
+)
