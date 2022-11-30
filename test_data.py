f7d336b649f085c489def36a92500bc71c5dccd1
Ronak Patel <ronak2715@gmail.com>
2022-11-29 22:10:10 -0500

    updated

---
+++ test_data.py
@@ -0,0 +1,13 @@
+def gettestArtistInfo():
+    return {'external_urls': {'spotify': 'https://open.spotify.com/artist/1TbRSunWGZ46mqnapcWxrm'}, 
+            'followers': {'href': None, 'total': 9157290}, 
+            'genres': ['boy band', 'dance pop', 'pop', 'post-teen pop'],
+            'href': 'https://api.spotify.com/v1/artists/1TbRSunWGZ46mqnapcWxrm', 
+            "preview_url": "https://p.scdn.co/mp3-preview/e1befa7fcb9e2799b9018ef1e0716e05814011da?cid=774b29d4f13844c495f206cafdad9c86",
+            'id': '1TbRSunWGZ46mqnapcWxrm', 
+            'images': [
+                        {'height': 640,'url': "https://i.scdn.co/image/ab67616d0000b273ad40f4e70b2f2a2a6052d49d","width": 640},
+                        {"height": 300,"url": "https://i.scdn.co/image/ab67616d00001e02ad40f4e70b2f2a2a6052d49d","width": 300}, 
+                        { "height": 64,"url": "https://i.scdn.co/image/ab67616d00004851ad40f4e70b2f2a2a6052d49d","width": 64}
+                    ],
+            'name': 'Salim Merchant', 'popularity':1 , 'type': 'artist', 'uri': 'spotify:artist:1TbRSunWGZ46mqnapcWxrm'}
